e8e279105362dcc8e83a6fc6a65a3579d0338aa6
JoaoLucas <joao.santos@hivolve.com>
2020-05-22 16:02:18 +0100

    Modificaçoes Queries

--- phpSubmissions/phpSubmiteClient.php
+++ phpSubmissions/phpSubmiteClient.php
@@ -40,16 +40,83 @@
        "\nTem outro?-".$other."\n---------------------------";
   
   
- //Aqui Vai as verificações
- /**Verificar se o email já existe -BD */
- /**Verificar nif - Regex*/
- /**verificar telemovel - Regex*/
- //
+  // 1- Verify if email exists
+  $sqlVerifyEmail = "SELECT email FROM users WHERE email = '" . $email . "'";
+  $resultVerifyEmail = $conn->query($sqlVerifyEmail);
+  if ($resultVerifyEmail->num_rows > 0) 
+  {
+    // Tell user that that name is already taken
+    echo "The email is already registered.";
+  } 
+  else 
+  {
+    // 2- Verify if nif exists
+    $sqlNif = "SELECT nif FROM users WHERE email = '" . $email . "' AND nif = '".$nif."'";
+    $resultNif = $conn->query($sqlNif);
+    if ($resultNif->num_rows > 0)
+    {
+      // Tell user that that name is already taken
+      echo "The nif is already registered.";
+    }
+    else
+    {
+      // 3- Verify nif
+      if(!binif_isvalid($nif)
+      {
+        echo "BI invalido";
+      }
+      else
+      {
+        echo "BI válido";
+        // 4- Verify phoneNumber
+        if (!phonenumber_isvalid($cellphone))
+        {
+          echo "Numero de telefone inválido"
+        }
+        else 
+        {
+          // Dados validos, executar registo
+          $sqlCommand = "INSERT INTO users('".$email."','".$name."','".$birthDate."','".$locality."','".$nif."','".$cellphone."','".$club."','".$auto."','".$life."','".$health."','".$house."','".$other."')";
+          $result = $con->query($sqlCommand);
+          echo $result;
 
-  //Aqui vai o INSERT PARA A BASE DE DADOS
-  $sqlCommand = "INSERT INTO users('".$email."','".$name."','".$birthDate."','".$locality."','".$nif."','".$cellphone."','".$club."','".$auto."','".$life."','".$health."','".$house."','".$other."')";
+          echo "OK";
+        }
+      }
+    }
+  }
 
-  $result = $con->query($sqlCommand);
 
-  echo $result;
+function phonenumber_isvalid($nr)
+{
+  if(strlen($nr != 9 ))
+  {
+    return false
+  }
+  if ((substr(trim($nr), 0, 2) == '91') || (substr(trim($nr), 0, 2) == '92') || (substr(trim($nr), 0, 2) == '93')|| (substr(trim($nr), 0, 2) == '96'))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+function binif_isvalid($nr){
+  while (strlen($nr) < 9) {
+     $nr = "0" . $nr;
+  }
+
+  $calc = 9 * $nr[0] + 8 * $nr[1] + 7 * $nr[2] + 6 * $nr[3] + 5 * $nr[4] + 4 * $nr[5] + 3 * $nr[6] + 2 * $nr[7] + $nr[8];
+
+  if ($calc % 11 === 0) {
+     return true;
+  }
+  else if($nr[8] === 0 && ($calc + 10) % 11 === 0) {
+     return true;
+  }
+  else {
+     return false;
+  }
+}  
 ?>
--- phpSubmissions/phpSubmiteLogIn.php
+++ phpSubmissions/phpSubmiteLogIn.php
@@ -19,7 +19,17 @@
         "APENAS PARA DEBUG ONLY!!!!!".
        "\nnome-".$name. "\nemail-".$email."\n---------------------------";
   //Aqui Vai as verificações
-
+  $accountVerify = "SELECT email FROM users WHERE email = '".$email."'";
+  $accountVerifyResult = $conn->query($accountVerify);
+  
+  if($accountVerifyResult->num_rows == 0)
+  {
+      die("Email not found on database");
+  }
+  else
+  {
+     echo "OK";
+  }
 
   //Aqui vai o INSERT PARA A BASE DE DADOS
   //$sqlCommand = "INSERT INTO `jogador`(`name`, `email`, `highScore`) VALUES ('$user','chi@mail.com','$pontos')";
