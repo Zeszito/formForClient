7f46f446dbf597a990c7ecdfd4f3d92bf30ded71
JoaoLucas <joao.santos@hivolve.com>
2020-05-26 23:10:10 +0100

    Backend - login funcional

--- phpSubmissions/phpSubmiteLogIn.php
+++ phpSubmissions/phpSubmiteLogIn.php
@@ -1,57 +1,67 @@
 <?php
- //Repostas:
-    //OK - Eu passo para a roda
-    //Qualquer outra eu escrevo no topo da pagina
-    //Erros de php eu digo só "Desculpe Erro Interno Tente mais tarde"
-
-
-
-   //Aqui para o ficheiro que faz a conecção
-   //include "connection.php";
-  
+   // MAKE SQLI CONNECTION
    $conn = new mysqli("15.188.164.24", "root", "sayhitoevolution", "sabseg_database");
    if($conn->connect_error)
    {
       die("error:" . $conn->connect_error);
    } 
-   else
-   {
-    // echo "Connected!";
-   }
 
+   // POSTS
    $email = $_POST['emailVal'];
-
-   //TESTE
-   /*echo  "---------------------------\n".
-        "APENAS PARA DEBUG!!!!!".
-        "\nemail-".$email."\n---------------------------";*/
 
    //1-Verificar se email esta registado
    $accountVerify = "SELECT email FROM users WHERE email = '".$email."'";
    if($conn->query($accountVerify)->num_rows == 0)
    {
-      echo("NO-Este email não foi ainda registado");
+      die("NO-Este email não foi ainda registado");
    }
    else
    {
-      // 2-Verificar se já jogou
-      $selectReward = "SELECT reward FROM users WHERE email = '".$email."' AND reward != '0'";
-      if ($conn->query($selectReward)->num_rows > 0) 
+      // 2 testar club
+      $selectUserClub = "SELECT club FROM users WHERE email = '".$email."'";
+      if($conn->query($selectUserClub)->num_rows == 0)
       {
-         echo"NO-Voçê já jogou á roleta";
+         die("NO-Erro a encontrar club");
       }
       else
       {
-         $clubSelect = "SELECT `club` FROM `users` WHERE email = '" . $email . "'";
-         $clubResult = $conn->query($clubSelect);
-         if ( $clubResult->num_rows == 0) 
+         // 3- Verificar se o club esta na data base
+         $userClub = mysqli_fetch_row($conn->query($selectUserClub))[0];
+         $selectClub = "SELECT `sponsored` FROM `clubs` WHERE `name` = '".$userClub."'";
+         if($conn->query($selectClub)->num_rows == 0)
          {
-            echo "NO-Error getting club";
+            die("NO-Erro a encontrar club do utilizador na db");
          }
          else
          {
-            $club = mysqli_fetch_row($clubResult);
-            echo "OK-".$club[0];
+            // 4- Verificar se o club é valido
+            if(mysqli_fetch_row($conn->query($selectClub))[0] == 0)
+            {
+               die("NO-Club não patrocionado pela sabseg");
+            }
+            else
+            {
+               // 5-Verificar se já jogou
+               $selectReward = "SELECT reward FROM users WHERE email = '".$email."' AND reward != '0'";
+               if ($conn->query($selectReward)->num_rows > 0) 
+               {
+                  die("NO-Voçê já jogou á roleta");
+               }
+               else
+               {
+                  $clubSelect = "SELECT `club` FROM `users` WHERE email = '" . $email . "'";
+                  $clubResult = $conn->query($clubSelect);
+                  if ( $clubResult->num_rows == 0) 
+                  {
+                     die("NO-Error getting club");
+                  }
+                  else
+                  {
+                     $club = mysqli_fetch_row($clubResult);
+                     echo "OK-".$club[0];
+                  }
+               }
+            }
          }
       }
    }
