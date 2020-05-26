265223576804019ae5331727f695195730e69ccf
Moises <liricractos@gmail.com>
2020-05-26 20:45:21 +0100

    Distritos adiconados

--- phpComponents/form.php
+++ phpComponents/form.php
@@ -15,10 +15,26 @@
                           <input type="date" class="form-control form-control-sm" name="dataNascimentoVal" >
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="Localidade">Localidade</label>
+                            <label for="Localidade">Distrito</label>
                             <select name="localidadeVal" id="LocalidadeVal" class="form-control form-control-sm">
-                              <option selected>Aveiro</option>
-                              <option>Porto</option>
+                              <option selected value="Aveiro">Aveiro</option>
+                              <option value="Beja">Beja</option>
+                              <option value="Braga">Braga</option>
+                              <option value="Bragança">Bragança</option>
+                              <option value="Castelo Branco">Castelo Branco</option>
+                              <option value="Coimbra">Coimbra</option>
+                              <option value="Évora">Évora</option>
+                              <option value="Faro">Faro</option>
+                              <option value="Guarda">Guarda</option>
+                              <option value="Leiria">Leiria</option>
+                              <option value="Lisboa">Lisboa</option>
+                              <option value="Portalegre">Portalegre</option>
+                              <option value="Porto">Porto</option>
+                              <option value="Santarém">Santarém</option>
+                              <option value="Setúbal">Setúbal</option>
+                              <option value="Viana do Castelo">Viana do Castelo</option>
+                              <option value="Vila Real">Vila Real</option>
+                              <option value="Viseu">Viseu</option>
                             </select>
                         </div>
                         <div class="form-group col-md-4">
