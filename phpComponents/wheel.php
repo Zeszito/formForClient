5c9df20351460ac669e6d6df26f211ff79f75d3a
Moises <liricractos@gmail.com>
2020-05-27 12:44:32 +0100

    img change

--- phpComponents/wheel.php
+++ phpComponents/wheel.php
@@ -4,7 +4,7 @@
 
     <div class="row">
         <div class="col-8 offset-2 col-md-4 offset-md-4 mt-4">
-            <img class="img-fluid" id="roda" src="images\roleta_default.png" alt="">
+            <img class="img-fluid" id="roda" src="images\rodas\default.png" alt="">
             <img class="img-fluid" id="rodaCentro" src="images\roda.png" alt="">
             <img class="img" id="seta" src="images\point.png" alt="">
 
