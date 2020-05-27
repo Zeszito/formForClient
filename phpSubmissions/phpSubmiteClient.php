7f890b05ef022cf77aaa52e14b321f9f7bd90aa3
JoaoLucas <joao.santos@hivolve.com>
2020-05-27 21:17:46 +0100

    Backend- rewards full name and clubs rewards count

--- phpSubmissions/phpSubmiteClient.php
+++ phpSubmissions/phpSubmiteClient.php
@@ -1,125 +1,125 @@
 <?php header('Content-Type: charset=utf-8');
-
-setlocale(LC_ALL, 'Portuguese_Portugal.1252');
-
-// Make connection to database
-$conn = new mysqli("15.188.164.24", "root", "sayhitoevolution", "sabseg_database");
-if($conn->connect_error)
-{
-   die("error:" . $conn->connect_error);
-} 
-
-mysqli_set_charset($conn, "utf8"); // TA-DA
-
-$name = $_POST['nomeVal'];
-$email = $_POST['emailVal'];
-$birthDate = $_POST['dataNascimentoVal'];
-$locality = $_POST['localidadeVal'];
-$nif = $_POST['nifVal'];
-$cellphone = $_POST['telemovelVal'];
-$club = $_POST['clubVal'];
-$auto = (isset($_POST['autoVal'])) ? 1: 0;
-$life = (isset($_POST['vidaVal'])) ? 1: 0;
-$health = (isset($_POST['saudeVal'])) ? 1: 0;
-$house = (isset($_POST['casaVal'])) ? 1: 0;
-$other = (isset($_POST['outroVal'])) ? 1: 0;
-//TESTE
- //echo  "---------------------------\n".
- //       "APENAS PARA DEBUG ONLY!!!!!".
- //      "\nnome-".$name. "\nemail-".$email."\ndataNas-".$birthDate."\nlocal-".$locality."\nnif-".$nif.
- //      "\ntelemo-".$cellphone."\nclub-".$club."\nTem auto?-".$auto."\nTem vida?-".$life."\nTem saude?-".$health."\nTem casa?-".$house.
- //     "\nTem outro?-".$other."\n---------------------------";
   
+  setlocale(LC_ALL, 'Portuguese_Portugal.1252');
   
-// 1- Verify if email exists
-$sqlVerifyEmail = "SELECT email FROM users WHERE email = '" . $email . "'";
-if ($conn->query($sqlVerifyEmail)->num_rows > 0) 
-{
-  // Tell user that that name is already taken
-  echo "NO-The email is already registered.";
-} 
-else 
-{
-  // 2- Verify if email is valid
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+  // Make connection to database
+  $conn = new mysqli("15.188.164.24", "root", "sayhitoevolution", "sabseg_database");
+  if($conn->connect_error)
   {
-    echo("NO-Not a valid email address.\n");
+     die("error:" . $conn->connect_error);
+  } 
+  
+  mysqli_set_charset($conn, "utf8"); // TA-DA
+  
+  $name = $_POST['nomeVal'];
+  $email = $_POST['emailVal'];
+  $birthDate = $_POST['dataNascimentoVal'];
+  $locality = $_POST['localidadeVal'];
+  $nif = $_POST['nifVal'];
+  $cellphone = $_POST['telemovelVal'];
+  $club = $_POST['clubVal'];
+  $auto = (isset($_POST['autoVal'])) ? 1: 0;
+  $life = (isset($_POST['vidaVal'])) ? 1: 0;
+  $health = (isset($_POST['saudeVal'])) ? 1: 0;
+  $house = (isset($_POST['casaVal'])) ? 1: 0;
+  $other = (isset($_POST['outroVal'])) ? 1: 0;
+  //TESTE
+   //echo  "---------------------------\n".
+   //       "APENAS PARA DEBUG ONLY!!!!!".
+   //      "\nnome-".$name. "\nemail-".$email."\ndataNas-".$birthDate."\nlocal-".$locality."\nnif-".$nif.
+   //      "\ntelemo-".$cellphone."\nclub-".$club."\nTem auto?-".$auto."\nTem vida?-".$life."\nTem saude?-".$health."\nTem casa?-".$house.
+   //     "\nTem outro?-".$other."\n---------------------------";
+    
+    
+  // 1- Verify if email exists
+  $sqlVerifyEmail = "SELECT email FROM users WHERE email = '" . $email . "'";
+  if ($conn->query($sqlVerifyEmail)->num_rows > 0) 
+  {
+    // Tell user that that name is already taken
+    echo "NO-The email is already registered.";
   } 
   else 
   {
-    // 3- Verify if nif exists
-    $sqlNif = "SELECT nif FROM users WHERE email = '" . $email . "' AND nif = '".$nif."'";
-    if ($conn->query($sqlNif)->num_rows > 0)
+    // 2- Verify if email is valid
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
     {
-      // Tell user that that name is already taken
-      echo "NO-The nif is already registered.";
-    }
-    else
+      echo("NO-Not a valid email address.\n");
+    } 
+    else 
     {
-      // 4- Verify nif
-      if(!binif_isvalid($nif))
+      // 3- Verify if nif exists
+      $sqlNif = "SELECT nif FROM users WHERE email = '" . $email . "' AND nif = '".$nif."'";
+      if ($conn->query($sqlNif)->num_rows > 0)
       {
-        echo "NO-BI invalido";
+        // Tell user that that name is already taken
+        echo "NO-The nif is already registered.";
       }
       else
       {
-        // 5- Verify phoneNumber
-        if (!phonenumber_isvalid($cellphone))
+        // 4- Verify nif
+        if(!binif_isvalid($nif))
         {
-          echo "NO-Numero de telefone inválido";
+          echo "NO-BI invalido";
         }
-        else 
+        else
         {
-          // Dados validos, executar registo
-          $sqlCommand = "INSERT INTO `users`(`email`, `name`, `birthDate`, `locality`, `nif`, `cellphone`, `club`, `auto`, `life`, `health`, `house`, `other`, `reward`) 
-          VALUES ('".$email."','".$name."','".$birthDate."','".$locality."','".$nif."','".$cellphone."','".$club."','".$auto."','".$life."','".$health."','".$house."','".$other."','0')";
-          if ($conn->query($sqlCommand) === TRUE) 
+          // 5- Verify phoneNumber
+          if (!phonenumber_isvalid($cellphone))
           {
-            echo "OK-";
+            echo "NO-Numero de telefone inválido";
           }
-          else
+          else 
           {
-            echo "NO-Não foi possivel inserir os dados na base de dados!";
+            // Dados validos, executar registo
+            $sqlCommand = "INSERT INTO `users`(`email`, `name`, `birthDate`, `locality`, `nif`, `cellphone`, `club`, `auto`, `life`, `health`, `house`, `other`, `reward`) 
+            VALUES ('".$email."','".$name."','".$birthDate."','".$locality."','".$nif."','".$cellphone."','".$club."','".$auto."','".$life."','".$health."','".$house."','".$other."','0')";
+            if ($conn->query($sqlCommand) === TRUE) 
+            {
+              echo "OK-";
+            }
+            else
+            {
+              echo "NO-Não foi possivel inserir os dados na base de dados!";
+            }
           }
         }
       }
     }
   }
-}
-
-
-function phonenumber_isvalid($nr)
-{
-  if(mb_strlen($nr) != 9)
+  
+  
+  function phonenumber_isvalid($nr)
   {
-    return false;
+    if(mb_strlen($nr) != 9)
+    {
+      return false;
+    }
+    if ((substr(trim($nr), 0, 2) == '91') || (substr(trim($nr), 0, 2) == '92') || (substr(trim($nr), 0, 2) == '93')|| (substr(trim($nr), 0, 2) == '96'))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
-  if ((substr(trim($nr), 0, 2) == '91') || (substr(trim($nr), 0, 2) == '92') || (substr(trim($nr), 0, 2) == '93')|| (substr(trim($nr), 0, 2) == '96'))
+  function binif_isvalid($nr)
   {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-function binif_isvalid($nr)
-{
-  while (strlen($nr) < 9) 
-  {
-     $nr = "0" . $nr;
-  }
-
-  $calc = 9 * $nr[0] + 8 * $nr[1] + 7 * $nr[2] + 6 * $nr[3] + 5 * $nr[4] + 4 * $nr[5] + 3 * $nr[6] + 2 * $nr[7] + $nr[8];
-
-  if ($calc % 11 === 0) {
-     return true;
-  }
-  else if($nr[8] === 0 && ($calc + 10) % 11 === 0) {
-     return true;
-  }
-  else {
-     return false;
-  }
-}  
+    while (strlen($nr) < 9) 
+    {
+       $nr = "0" . $nr;
+    }
+  
+    $calc = 9 * $nr[0] + 8 * $nr[1] + 7 * $nr[2] + 6 * $nr[3] + 5 * $nr[4] + 4 * $nr[5] + 3 * $nr[6] + 2 * $nr[7] + $nr[8];
+  
+    if ($calc % 11 === 0) {
+       return true;
+    }
+    else if($nr[8] === 0 && ($calc + 10) % 11 === 0) {
+       return true;
+    }
+    else {
+       return false;
+    }
+  }  
 ?>
--- phpSubmissions/phpSubmiteReward.php
+++ phpSubmissions/phpSubmiteReward.php
@@ -1,19 +1,21 @@
 <?php header('Content-Type: charset=utf-8');
 
-setlocale(LC_ALL, 'Portuguese_Portugal.1252');
+   setlocale(LC_ALL, 'Portuguese_Portugal.1252');
 
- use PHPMailer\PHPMailer\PHPMailer;
- use PHPMailer\PHPMailer\Exception;
- 
- require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/Exception.php';
- require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/PHPMailer.php';
- require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/SMTP.php';
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+   
+   require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/Exception.php';
+   require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/PHPMailer.php';
+   require '/opt/lampp/htdocs/roleta/phpSubmissions/phpmailer/src/SMTP.php';
  
    $conn = new mysqli("15.188.164.24", "root", "sayhitoevolution", "sabseg_database");
    if($conn->connect_error)
    {
       die("error:" . $conn->connect_error);
    } 
+
+   mysqli_set_charset($conn, "utf8"); // TA-DA
 
    $reward = $_POST['rewardVal'];
    $email = $_POST['emailVal'];
@@ -29,116 +31,124 @@ setlocale(LC_ALL, 'Portuguese_Portugal.1252');
       $rewardConn = $conn->query($getReward);
       if ($rewardConn->num_rows == 0)
       {
-         echo "NO-Erro a encontrar reward - email nao valido";
+         echo "NO-Erro a encontrar reward";
       }
       else
       {
-         
          // Current user reward
          $sqlReward = mysqli_fetch_row($conn->query($getReward))[0];
-
-         //Send email
-         $mail = new PHPMailer();
-         $mail->IsSMTP(); // enable SMTP
-         //$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-         $mail->SMTPAuth = true; // authentication enabled
-         $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-         //$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
-         $mail->Host = "smtp.gmail.com";
-         //$mail->Host = "smtp.live.com";
-         $mail->Port = 465; // 465 or 587
-         $mail->IsHTML(true);
-         $mail->Username = "hivolvept@gmail.com";
-         $mail->Password = "sayhitoevolutionemail";
-         $mail->SetFrom("hivolvept@gmail.com");
-         $mail->Subject = "Celebramos Futebol | SABSEG Seguros: Roleta Digital";
-
-         // CAMISOLA
-         if($sqlReward == "camisola")
+         // Get reward fullname
+         $getRewardName = "SELECT reward_name FROM rewards WHERE index_name = '" . $sqlReward . "'";
+         if($conn->query($getRewardName)->num_rows == 0)
          {
-            $mail->Body = "<pre>Olá,
-
-Desde já agradecemos a tua participação na Roleta Digital SABSEG!
-O prémio que ganhaste, através da nossa Roleta, foi: Camisola Oficial
-
-Para receberes o prémio, por favor, responde a este e-mail indicando-nos o tamanho pretendido e a morada para onde devemos enviar o prémio. Todos os prémios vão ser enviados por correio.
-
-Após recebermos o teu e-mail vamos entrar em contacto contigo confirmando o prémio e a indicação da morada. 
-
-Posteriormente, quando efetuarmos o envio para a tua morada, receberás, também, um e-mail com essa indicação!
-
-Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
-
-
-Junta-te a nós e vive experiências inesquecíveis.
-O futebol é a paixão que nos une.
-
-<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
+            echo ("NO-Não encontro recompensa na db");
          }
-         // CAMISOLA ALTERNATIVA
-         else if($sqlReward == "camisolaAlt")
-         {
-            $mail->Body = "<pre>Olá,
-
-Desde já agradecemos a tua participação na Roleta Digital SABSEG!
-O prémio que ganhaste, através da nossa Roleta, foi: Camisola Oficial Alternativa
-
-Para receberes o prémio, por favor, responde a este e-mail indicando-nos o tamanho pretendido e a morada para onde devemos enviar o prémio. Todos os prémios vão ser enviados por correio.
-
-Após recebermos o teu e-mail vamos entrar em contacto contigo confirmando o prémio e a indicação da morada. 
-
-Posteriormente, quando efetuarmos o envio para a tua morada, receberás, também, um e-mail com essa indicação!
-
-Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
-
-
-Junta-te a nós e vive experiências inesquecíveis.
-O futebol é a paixão que nos une.
-
-<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
-         }
-         // SEGURO AUTOMOVEL
-         else if($sqlReward == "seguro")
-         {
-            $mail->Body = "<pre>Olá,
-
-Desde já agradecemos a tua participação na Roleta Digital SABSEG!
-O prémio que ganhaste, através da nossa Roleta, foi: Seguro Automóvel (oferta da primeira anuidade)
-
-Para receberes o prémio, por favor, responde a este e-mail demonstrando interesse pelo mesmo. Após recebermos o teu e-mail vamos entrar em contacto contigo! 
-
-Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
-
-Junta-te a nós e vive experiências inesquecíveis.
-O futebol é a paixão que nos une.
-
-<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
-         }
-         // CONSULTORIA DE SEGUROS
-         else if($sqlReward == "consultoria")
-         {
-            $mail->Body = "<pre>Olá,
-
-Desde já agradecemos a tua participação na Roleta Digital SABSEG!
-O prémio que ganhaste, através da nossa Roleta, foi: Consultoria de todos os teus seguros
-            
-Para receberes o prémio, por favor, responde a este e-mail demonstrando interesse pelo mesmo. Após recebermos o teu e-mail vamos entrar em contacto contigo! 
-            
-Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
-            
-Junta-te a nós e vive experiências inesquecíveis.
-O futebol é a paixão que nos une.
-
-            
-<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
-         }
-         // OTHER
          else
          {
-            $mail->Body = "<pre>Olá,
+            $sqlRewardName = mysqli_fetch_row($conn->query($getRewardName))[0];
+
+            //Send email
+            $mail = new PHPMailer();
+            $mail->IsSMTP(); // enable SMTP
+            //$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+            $mail->SMTPAuth = true; // authentication enabled
+            $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+            //$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+            $mail->Host = "smtp.gmail.com";
+            //$mail->Host = "smtp.live.com";
+            $mail->Port = 465; // 465 or 587
+            $mail->IsHTML(true);
+            $mail->Username = "hivolvept@gmail.com";
+            $mail->Password = "sayhitoevolutionemail";
+            $mail->SetFrom("hivolvept@gmail.com");
+            $mail->Subject = "Celebramos Futebol | SABSEG Seguros: Roleta Digital";
+
+            // CAMISOLA
+            if($sqlReward == "camisola")
+            {
+               $mail->Body = "<pre>Olá,
 
 Desde já agradecemos a tua participação na Roleta Digital SABSEG!
-O prémio que ganhaste, através da nossa Roleta, foi: $sqlReward 
+O prémio que ganhaste, através da nossa Roleta, foi: $sqlRewardName
+
+Para receberes o prémio, por favor, responde a este e-mail indicando-nos o tamanho pretendido e a morada para onde devemos enviar o prémio. Todos os prémios vão ser enviados por correio.
+
+Após recebermos o teu e-mail vamos entrar em contacto contigo confirmando o prémio e a indicação da morada. 
+
+Posteriormente, quando efetuarmos o envio para a tua morada, receberás, também, um e-mail com essa indicação!
+
+Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
+
+
+Junta-te a nós e vive experiências inesquecíveis.
+O futebol é a paixão que nos une.
+
+<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
+            }
+            // CAMISOLA ALTERNATIVA
+            else if($sqlReward == "camisolaAlt")
+            {
+               $mail->Body = "<pre>Olá,
+
+Desde já agradecemos a tua participação na Roleta Digital SABSEG!
+O prémio que ganhaste, através da nossa Roleta, foi: $sqlRewardName
+
+Para receberes o prémio, por favor, responde a este e-mail indicando-nos o tamanho pretendido e a morada para onde devemos enviar o prémio. Todos os prémios vão ser enviados por correio.
+
+Após recebermos o teu e-mail vamos entrar em contacto contigo confirmando o prémio e a indicação da morada. 
+
+Posteriormente, quando efetuarmos o envio para a tua morada, receberás, também, um e-mail com essa indicação!
+
+Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
+
+
+Junta-te a nós e vive experiências inesquecíveis.
+O futebol é a paixão que nos une.
+
+<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
+            }
+            // SEGURO AUTOMOVEL
+            else if($sqlReward == "seguro")
+            {
+               $mail->Body = "<pre>Olá,
+
+Desde já agradecemos a tua participação na Roleta Digital SABSEG!
+O prémio que ganhaste, através da nossa Roleta, foi: $sqlRewardName (oferta da primeira anuidade)
+
+Para receberes o prémio, por favor, responde a este e-mail demonstrando interesse pelo mesmo. Após recebermos o teu e-mail vamos entrar em contacto contigo! 
+
+Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
+
+Junta-te a nós e vive experiências inesquecíveis.
+O futebol é a paixão que nos une.
+
+<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
+            }
+            // CONSULTORIA DE SEGUROS
+            else if($sqlReward == "consultoria")
+            {
+               $mail->Body = "<pre>Olá,
+
+Desde já agradecemos a tua participação na Roleta Digital SABSEG!
+O prémio que ganhaste, através da nossa Roleta, foi: $sqlRewardName
+            
+Para receberes o prémio, por favor, responde a este e-mail demonstrando interesse pelo mesmo. Após recebermos o teu e-mail vamos entrar em contacto contigo! 
+            
+Mais uma vez, muito obrigado por participares na Roleta Digital SABSEG.
+            
+Junta-te a nós e vive experiências inesquecíveis.
+O futebol é a paixão que nos une.
+
+            
+<b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
+            }
+            // OTHER
+            else
+            {
+               $mail->Body = "<pre>Olá,
+
+Desde já agradecemos a tua participação na Roleta Digital SABSEG!
+O prémio que ganhaste, através da nossa Roleta, foi: $sqlRewardName 
 
 Para receberes o prémio, por favor, responde a este e-mail indicando-nos a morada para onde devemos enviar o prémio. Todos os prémios vão ser enviados por correio.
 
@@ -153,16 +163,17 @@ O futebol é a paixão que nos une.
 
 
 <b>Celebramos o Futebol | SABSEG Seguros</b></pre>";
-         }
+            }
 
-         $mail->AddAddress($email);
-         if(!$mail->Send()) 
-         {
-           echo "NO-Mailer error: " . $mail->ErrorInfo;
-         } 
-         else 
-         {
-            echo "OK-";
+            $mail->AddAddress($email);
+            if(!$mail->Send()) 
+            {
+              echo "NO-Mailer error: " . $mail->ErrorInfo;
+            } 
+            else 
+            {
+               echo "OK-";
+            }
          }
       }
    }
