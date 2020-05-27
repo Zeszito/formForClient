cc5cd2e36a456784805d231a40a26f6c8c807664
Moises <liricractos@gmail.com>
2020-05-27 19:00:20 +0100

    fixing i hope

--- phpComponents/botBanner.php
+++ phpComponents/botBanner.php
@@ -8,32 +8,28 @@
                 <li><a class="text-muted" href="#">Contactos</a></li>
             </ul>
         </div>
-        <div class="col-12 col-sm offset-sm-1">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Resource</a></li>
-                <li><a class="text-muted" href="#">Resource name</a></li>
-                <li><a class="text-muted" href="#">Another resource</a></li>
-                <li><a class="text-muted" href="#">Final resource</a></li>
-            </ul>
+        <div class="col-12 col-sm">
+            <div class="row">
+                <div class="col mb-4 mb-md-0">
+                    <img class="img-resposnive lgDark mx-auto d-block" src="images\footerImg\Logo_Icon_Dark.svg" alt="">
+                </div>
+            </div>
         </div>
         <div class="col-12 col-sm offset-sm-1">
             <div class="row">
                 <div class="col noPadLR">
-                    <img class="img-fluid" src="images\footerImg\footer_facebook.png" alt="">
+                <a href="#"><img class="img-fluid" src="images\footerImg\footer_facebook.png" alt=""></a>
                 </div>
                 <div class="col noPadLR">
-                    <img class="img-fluid" src="images\footerImg\footer_instagram_hover.png" alt="">
+                <a href="#"> <img class="img-fluid" src="images\footerImg\footer_instagram_hover.png" alt=""></a>
                 </div>
                 <div class="col noPadLR">
-                    <img class="img-fluid" src="images\footerImg\footer_linkedin.png" alt="">
+                <a href="#">  <img class="img-fluid" src="images\footerImg\footer_linkedin.png" alt=""></a>
                 </div>
                 <div class="col noPadLR">
-                    <img class="img-fluid" src="images\footerImg\footer_twitter.png" alt="">
+                <a href="#">  <img class="img-fluid" src="images\footerImg\footer_twitter.png" alt=""></a>
                 </div>
             </div>
-
-
         </div>
     </div>
 </footer>
